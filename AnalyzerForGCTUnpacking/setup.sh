65547f64057638fa5e975c9efee02fb70629ba1e
Chad <chad.flores@cern.ch>
2015-11-06 11:49:29 +0100

    typo

--- AnalyzerForGCTUnpacking/setup.sh
+++ AnalyzerForGCTUnpacking/setup.sh
@@ -3,8 +3,8 @@ cmsrel CMSSW_7_5_3_patch1
 cd CMSSW_7_5_3_patch1/src
 cmsenv
 git cms-merge-topic -u CmsHI:forest_$CMSSW_VERSION
-mkdir Analyzer
-cd Analyzer
+mkdir Analyzers
+cd Analyzers
 git clone -b AnalyzerForGCTunpacking git@github.com:richard-cms/L1UpgradeAnalyzer.git
 scram b -j8
 
